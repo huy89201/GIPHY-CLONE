885769ddbbd290580c9b4c1966c3c636d1551779
huy89201 <levanhuy08092001@gmail.com>
2024-03-22 15:39:53 +0700

    Deploy

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   env: {
     GIPHY_API_KEY: "rJLTbsZaG1XIpkj8Qk1KcQTUV7oSQ1aU",
   },
-  output: 'export',
 };
 
 module.exports = nextConfig;
